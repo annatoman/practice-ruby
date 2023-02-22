bb0f70a0828a04e23b30eb2d015145502d8c2933
Anna <annamtoman@gmail.com>
2023-02-22 10:05:45 -0600

    adds ruby and java symbols

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,13 @@
-flower_one = {name: "delphinium" color: "blue", price: 3.95}
-flower_two = {:name => "rose", :color => "pink", :price => 5}
-flower_three = {name: "peony", color: "coral", price: 6}
+# Ruby Syntax
 
-"Anna has a flower shop where she sells #{name}'s for "
+flower_one = {:name => "delphinium" :color => "blue", :price => 3}
+flower_two = {:name => "rose", :color => "pink", :price => 5}
+flower_three = {:name => "peony", :color => "coral", :price => 6}
+
+puts "Anna has a flower shop where she sells #{flower_two[:name]}'s for #{flower_two[:price]}"
+
+# Java Syntax
+
+flower_one = {name: "delphinium" color: "blue", price: 3.95}
+flower_two = {name: "rose", color: "pink", price: 5}
+flower_three = {name: "peony", color: "coral", price: 6}
