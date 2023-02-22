6f139870bf67d339587e085dc92d3aef0346f4e1
Anna <annamtoman@gmail.com>
2023-02-22 13:53:58 -0600

    adds manager.rb

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,40 +1,52 @@
-class Car
+class Vehicle
     def initialize
-      @speed = 0
-      @direction = 'north'
+        @speed = 0
+        @direction = 'north'
+      end
+    
+      def brake
+        @speed = 0
+      end
+    
+      def accelerate
+        @speed += 10
+      end
+    
+      def turn(new_direction)
+        @direction = new_direction
+      end
+end
+
+class Car < Vehicle
+
+    attr_writer :make, :model, :color
+
+    def initialize(input_options)
+        @make = input_options[:make]
+        @model = input_options[:model]
+        @color = input_options[:color]
     end
-  
-    def brake
-      @speed = 0
-    end
-  
-    def accelerate
-      @speed += 10
-    end
-  
-    def turn(new_direction)
-      @direction = new_direction
-    end
-  
+
     def honk_horn
       puts "Beeeeeeep!"
     end
+
   end
   
-  class Bike < Car
+  class Bike < Vehicle
   
+    def initialize(input_options)
+        super
+    end
+
     def ring_bell
       puts "Ring ring!"
     end
 
   end
 
-  car = Car.new
-  bike = Bike.new
+  car = Car.new(make: "Honda", model: "Civic", color: "Silver")
+  bike = Bike.new(make: "Schwinn", model: "Pontera", color: "Red")
 
-  car.speed(10)
-  car.accelerate
-  p car.speed
-
-  p car.honk_horn
-  p bike.ring_bell
+  p bike
+  p car
