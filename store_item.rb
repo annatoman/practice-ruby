346a4f9cac84a3e8ad9f3fa62f50548095a192a6
Anna <annamtoman@gmail.com>
2023-02-22 09:53:11 -0600

    Adds syntax comments

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 flower_one = {name: "delphinium" color: "blue", price: 3.95}
 flower_two = {:name => "rose", :color => "pink", :price => 5}
 flower_three = {name: "peony", color: "coral", price: 6}
+
+"Anna has a flower shop where she sells #{name}'s for "
