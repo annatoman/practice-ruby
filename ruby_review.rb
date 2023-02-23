556709f3ca84783adbfb53875d05e95809030355
Anna <annamtoman@gmail.com>
2023-02-23 13:45:35 -0600

    adds hashes exercise

--- ruby_review.rb
+++ ruby_review.rb
@@ -1,20 +1,45 @@
-puts "What are your five favorite foods?"
+# puts "What are your five favorite foods?"
 
-fave_foods = []
+# fave_foods = []
 
-5.times do
-    food = gets.chomp
-    fave_foods << food
-end
+# 5.times do
+#     food = gets.chomp
+#     fave_foods << food
+# end
+
+# # i = 0
+# # 5.times do
+# #     puts "I love #{fave_foods[i]}"
+# #     i = i + 1
+# # end
 
 # i = 0
 # 5.times do
-#     puts "I love #{fave_foods[i]}"
+#     puts "#{i + 1}. #{fave_foods[i]}"
 #     i = i + 1
 # end
 
-i = 0
+info = []
+
 5.times do
-    puts "#{i + 1}. #{fave_foods[i]}"
+    puts "Enter a first name, last name and email: "
+    people = {}
+    first_name = gets.chomp
+    last_name = gets.chomp
+    email = gets.chomp
+    account = rand(10 ** 10)
+    people["first_name"] = first_name
+    people["last_name"] = last_name
+    people["email"] = email
+    people["account"] = account
+    info << people
+end
+
+i = 0
+while i < info.length
+    puts "FIRST NAME: #{info[i]["first_name"]}"
+    puts "LAST NAME: #{info[i]["last_name"]}"
+    puts "EMAIL: #{info[i]["email"]}"
+    puts "ACCOUNT #: #{info[i]["account"]}"
     i = i + 1
 end
