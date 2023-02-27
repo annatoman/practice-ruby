37c8899d18ee86ecd68e917613ff332f8273318a
Anna <annamtoman@gmail.com>
2023-02-27 09:54:05 -0600

    creates methods_practice_problems.rb

---
+++ methdods_practice_problems.rb
@@ -0,0 +1,46 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+puts "Problem 1: "
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Problem 2: "
+
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+puts "Problem 3: "
+
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+puts "Problem 4: "
+
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+puts "Problem 5: "
+
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+puts "Problem 6: "
+
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+puts "Problem 7: "
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+puts "Problem 8: "
+
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+puts "Problem 9: "
+
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+puts "Problem 10: "
