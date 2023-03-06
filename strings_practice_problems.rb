35cb4aaafdd3b4c6ed75196e283ae8429212b197
Anna <annamtoman@gmail.com>
2023-03-05 20:07:03 -0600

    adds answers again

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -2,51 +2,98 @@
 
 puts "Problem 1: "
 
-first = "Anna"
-second = "Toman"
+first_name = "Jenny"
+last_name = "Holt"
 
-
+puts first_name + " " + last_name
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
 puts "Problem 2: "
 
+first_name = "Jenny"
+last_name = "Holt"
+
+puts "#{first_name} #{last_name}"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
 puts "Problem 3: "
 
+# puts "Input a word:"
+# word = gets.chomp
+
+# if word == "marco"
+#     p "polo"
+# end
+
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
 puts "Problem 4: "
 
+color_one = "blue"
+color_two = "periwinkle"
+color_three = "purple"
+
+puts color_one + ", " + color_two + ", " + color_three
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
 puts "Problem 5: "
 
+color_one = "blue"
+color_two = "periwinkle"
+color_three = "purple"
+
+puts "#{color_one}, #{color_two}, #{color_three}"
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
 puts "Problem 6: "
 
+# puts "Enter a name:"
+# name = gets.chomp
+# if name != "Santa"
+#     puts "You're not Santa"
+# end
 
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
 puts "Problem 7: "
 
+book_title = "Americanah"
+book_author = "Chimamanda Ngozi Adichi"
+
+puts book_title + " " + book_author
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
 puts "Problem 8: "
 
+book_title = "Americanah"
+book_author = "Chimamanda Ngozi Adichi"
 
+puts "#{book_title} #{book_author}"
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
 puts "Problem 9: "
 
+# puts "Enter a password:"
+# pass = gets.chomp
+
+# if pass == "Joshua"
+#     puts "Shall we play a game?"
+# else
+#     puts "Access denied."
+# end
 
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
 puts "Problem 10: "
+
+city_one = "Chicago"
+city_two = "New York"
+city_three = "LA"
+
+puts city_one + ", " + city_two + ", " + city_three
