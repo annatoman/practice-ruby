ac9617f56c4247508403f4df37b56faa4d365622
Anna <annamtoman@gmail.com>
2023-03-06 10:42:54 -0600

    adds hash practice problems

---
+++ hash_practice_problems.rb
@@ -0,0 +1,36 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+puts "Problem One:"
+
+contact = {"first_name" => "Kate", "last_name" => "Middleton", "email" =>"kate@example.com"}
+
+p contact["first_name"]
+p contact["last_name"]
+p contact["email"]
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+puts "Problem Two:"
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+puts "Problem Three:"
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+puts "Problem Four:"
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+puts "Problem Five:"
+# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+puts "Problem Six:"
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+puts "Problem Seven:"
+# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+puts "Problem Eight:"
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+puts "Problem Nine:"
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+puts "Problem Ten:"
