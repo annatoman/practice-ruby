c949b9ac6390090cee30681943d476e3aad677e2
Anna <annamtoman@gmail.com>
2023-03-20 10:52:26 -0500

    adds a couple of answers for while loops

--- arrays_select_practice_problems.rb
+++ arrays_select_practice_problems.rb
@@ -1,29 +1,85 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
+# While Loop
+
+numbers = [2, 32, 80, 18, 12, 3]
+less_twenty = []
+i = 0
+while i < numbers.length
+    if numbers[i] < 20
+    less_twenty << numbers[i]
+    end
+    i += 1
+end
+
+p less_twenty
+
+# Each Do
+
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+# While Loop
+
+strings = ["winner", "winner", "chicken", "dinner"]
+w_strings = []
+i = 0
+while i < strings.length
+    if strings[i][0] == "w"
+        w_strings << strings[i]
+    end
+    i += 1
+end
+
+p w_strings
+
+# Each Do
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
+# While Loop
+# Each Do
+
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+# While Loop
+# Each Do
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
+# While Loop
+# Each Do
+
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+# While Loop
+# Each Do
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
+# While Loop
+# Each Do
+
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+# While Loop
+# Each Do
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+# While Loop
+# Each Do
+
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+# While Loop
+# Each Do
