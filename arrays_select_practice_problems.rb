ac781de21d61640a71c04d00d46006f9021294c3
Anna <annamtoman@gmail.com>
2023-03-24 09:56:12 -0500

    completes while loop answers

--- arrays_select_practice_problems.rb
+++ arrays_select_practice_problems.rb
@@ -40,46 +40,154 @@ p w_strings
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
 # While Loop
+
+# products = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# five = []
+# i = 0
+# while i < products.length
+#     product = products[i]
+#     if product[:price] > 5
+#         five << product
+#     end
+#     i += 0
+# end
+
+# p five
+
 # Each Do
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
 # While Loop
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+even_numbers = []
+i = 0
+while i < numbers.length
+    if numbers[i] % 2 == 0
+        even_numbers << numbers[i]
+    end
+    i += 1
+end
+
+p even_numbers
+
 # Each Do
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
 # While Loop
+
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+small_strings = []
+i = 0
+while i < strings.length
+    if strings[i].length < 4
+        small_strings << strings[i]
+    end
+    i += 1
+end
+
+p small_strings
+
 # Each Do
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+# For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
 # While Loop
+
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+small_hashes = []
+i = 0
+while i < hashes.length
+    if hashes[i][:name].length < 6
+        small_hashes << hashes[i]
+    end
+    i += 1
+end
+
+p small_hashes
+
 # Each Do
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
 # While Loop
+
+numbers = [8, 23, 0, 44, 1980, 3]
+small_numbers = []
+i = 0
+while i < numbers.length
+    if numbers[i] < 10
+        small_numbers << numbers[i]
+    end
+    i += 1
+end
+
+p small_numbers
+
 # Each Do
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
-#     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+# For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
 # While Loop
+
+words = ["big", "little", "good", "bad"]
+b_words = []
+i = 0
+
+while i < words.length
+    if words[i][0] != "b"
+        b_words << words[i]
+    end
+    i += 1
+end
+
+p b_words
+
 # Each Do
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+# For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
 # While Loop
+
+prices = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+low_prices = []
+i = 0
+
+while i < prices.length
+    if prices[i][:price] < 10
+        low_prices << prices[i]
+    end
+    i += 1
+end
+
+p low_prices
+
 # Each Do
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
-#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+# For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
 # While Loop
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+odd_numbers = []
+i = 0
+
+while i < numbers.length
+    if numbers[i] % 2 == 1
+        odd_numbers << numbers[i]
+    end
+    i += 1
+end
+
+p odd_numbers
+
 # Each Do
